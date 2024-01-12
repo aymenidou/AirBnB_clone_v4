5c90e23c7dfaa1a6294d5331a8d509ce0773f860
Aymen Idouahman <aymanidou@gmail.com>
2024-01-13 00:25:37 +0100

    change fixes

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,15 @@
-$(document).ready(function() {
-    
-    var amenityList = [];
-
-    $('input[type="checkbox"]').change(function() {
-
-        var amenityID = $(this).data('amenity-id');
-
-        if ($(this).prop('checked')) {
-            amenityList.push(amenityID);
-        } else {
-            amenityList = amenityList.filter(function(id) {
-                return id !== amenityID;
-            });
-        }
-
-        $('div.Amenities h4').text('Amenities: ' + amenityList.join(', '));
-    });
+const $ = window.$;
+$(document).ready(function () {
+  let amenityList = [];
+  $('input[type="checkbox"]').change(function () {
+    const amenityID = $(this).data('amenity-id');
+    if ($(this).prop('checked')) {
+      amenityList.push(amenityID);
+    } else {
+      amenityList = amenityList.filter(function (id) {
+        return id !== amenityID;
+      });
+    }
+    $('div.Amenities h4').text('Amenities: ' + amenityList.join(', '));
+  });
 });
