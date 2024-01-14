394d2b5eebb77d56483c5ae371851e1289170f93
Aymen Idouahman <aymanidou@gmail.com>
2024-01-14 12:05:39 +0100

    fix changes

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,25 @@
 const $ = window.$;
 $(document).ready(function () {
   let amenityList = [];
+  // let amenityList = {};
   $('input[type="checkbox"]').change(function () {
-    const amenityID = $(this).data('amenity-id');
+    // get amenity id from attribute
+    const amenityID = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
     if ($(this).prop('checked')) {
-      amenityList.push(amenityID);
+      // if checkbox checked add to list
+      amenityList[amenityID] = amenityName;
+      // amenityList.push(amenityName);
     } else {
-      amenityList = amenityList.filter(function (id) {
-        return id !== amenityID;
-      });
+      // else remove amenity id from the list
+      delete amenityList[$(this).attr('data-id')];
+      // amenityList = amenityList.filter(function (name) {
+      //   return name !== amenityName;
+      // });
     }
-    $('div.Amenities h4').text('Amenities: ' + amenityList.join(', '));
+    // console.log(amenityList)
+    const names = Object.values(amenityList);
+    $('div.amenities h4').text(names.join(', '));
+    // $('div.amenities h4').text(amenityList.keysjoin(', '));
   });
 });
