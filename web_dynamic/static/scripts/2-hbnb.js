e38e4e52bdf3dc9dc11fce80c3261a41d6d6d919
Aymen Idouahman <aymanidou@gmail.com>
2024-01-14 14:03:57 +0100

    changes for task 2

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,37 @@
+const $ = window.$;
+$(document).ready(function () {
+  let amenityList = [];
+  // let amenityList = {};
+  $('input[type="checkbox"]').change(function () {
+    // get amenity id from attribute
+    const amenityID = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+    if ($(this).prop('checked')) {
+      // if checkbox checked add to list
+      amenityList[amenityID] = amenityName;
+      // amenityList.push(amenityName);
+    } else {
+      // else remove amenity id from the list
+      delete amenityList[$(this).attr('data-id')];
+      // amenityList = amenityList.filter(function (name) {
+      //   return name !== amenityName;
+      // });
+    }
+    // console.log(amenityList)
+    const names = Object.values(amenityList);
+    $('div.amenities h4').text(names.join(', '));
+    // $('div.amenities h4').text(amenityList.keysjoin(', '));
+  });
+
+  const API = "http://0.0.0.0:5001/api/v1/status/";
+  $.get(API, (data, status) => {
+    console.log(data);
+    console.log(status);
+    if (status === 'success' && data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+});
